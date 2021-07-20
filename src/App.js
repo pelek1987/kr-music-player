55f68a77c4240fa9a09d88d5273a5d09c4cc75f3
Pawel Sioda <pawel.sioda@gmail.com>
2021-07-20 05:47:49 +0200

    create heading component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './styles.css';
 
+function Heading() {
+ return (
+     <h1>Music Player</h1>
+ )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
-      <h2>Let's create a Music Player</h2>
+      <Heading />
+        <audio controls>
+            <source src={'https://examples.devmastery.pl/assets/audio/deadfro5h.mp3'} />
+        </audio>
     </div>
   );
 }
